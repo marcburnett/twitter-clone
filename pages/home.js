3ef9b37795116ff990c2c3e68b07e042f291cc4e
marcburnett <marc_burnett@yahoo.com>
2022-06-20 19:56:43 +0100

    first commit

---
+++ pages/home.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+import NewTweet from './components/NewTweet'
+import Tweets from './components/Tweets'
+import prisma from 'lib/prisma'
+import { getTweets } from 'lib/data.js'
+import LoadMore from './components/LoadMore'
+
+export default function Home({ initialTweets }) {
+  const [tweets, setTweets] = useState(initialTweets)
+
+  const router = useRouter()
+  const { data: session, status } = useSession()
+  const loading = status === 'loading'
+
+  if (loading) {
+    return null
+  }
+
+  if (!session) {
+    router.push('/')
+  }
+
+  if (session && !session.user.name) {
+    router.push('/setup')
+  }
+
+  return (
+    <>
+      <NewTweet tweets={tweets} setTweets={setTweets} />
+      <Tweets tweets={tweets} />
+      <LoadMore tweets={tweets} setTweets={setTweets} />
+    </>
+  )
+}
+
+export async function getServerSideProps() {
+  let tweets = await getTweets(prisma, 2)
+  tweets = JSON.parse(JSON.stringify(tweets))
+
+  return {
+    props: {
+      initialTweets: tweets,
+    },
+  }
+}
